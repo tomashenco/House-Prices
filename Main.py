666a5ce9c6ce48be42676df7c3dd37da30579056
tomashenco <dobrzycki.tomasz@gmail.com>
2016-01-15 15:55:28 +0000

    Searches the page and finds all the URLs

--- Main.py
+++ Main.py
@@ -1,5 +1,16 @@
 from WebSeeker import web_seeker
+from WebSeeker import read_page
 
-locations = ('York',)
+locations = ('Cambridge', 'York')
 
-web_seeker(locations)
+'''
+('London', 'Canterbury', 'Newbury', 'Bristol', 'Exeter',
+         'Ipswich', 'Cambridge', 'Northampton', 'Birmingham',
+         'Boston', 'Sheffield', 'Manchester', 'Liverpool',
+         'York', 'Lancaster', 'Middlesbrough', 'Carlisle', 'Newcastle')
+'''
+
+# web_seeker(locations)
+
+link = 'http://www.rightmove.co.uk/new-homes-for-sale/property-43327825.html'
+read_page(link)
--- WebSeeker.py
+++ WebSeeker.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 import requests
 
+
 def read_page(url):
-    pass
+    r = requests.get(url)
+    print r.text
 
 
 
@@ -19,21 +22,41 @@ def web_seeker(locations):
         # Check if opened
         assert "UK's number one" in driver.title
 
+        # Select city in search box and proceed
         search_form = driver.find_element_by_id('searchLocation')
         search_form.send_keys(city)
         search_form.send_keys(Keys.RETURN)
 
+        # Select search parameters
         radius = Select(driver.find_element_by_id('radius'))
         radius.select_by_index(10)  # 40 miles
+        min_price = Select(driver.find_element_by_id('minPrice'))
+        min_price.select_by_index(6)  # 100k
         max_price = Select(driver.find_element_by_id('maxPrice'))
-        max_price.select_by_index(32)  # 400k
+        max_price.select_by_index(27)  # 400k
         property_type = Select(driver.find_element_by_id('displayPropertyType'))
         property_type.select_by_index(1)  # houses only
         added = Select(driver.find_element_by_id('maxDaysSinceAdded'))
         added.select_by_index(1)  # last 24 hours
 
-        find_button = driver.find_element_by_id('submit')
-        find_button.click()
+        # Submit search paremeters
+        submit_button = driver.find_element_by_id('submit')
+        submit_button.click()
 
-        link = driver.find_element_by_xpath('//*[@href]')
-        print link.get_attribute('href')
+        # Look at all pages but now more than 100
+        index = 0
+        while index < 2:
+            # Look for all offers and get URLs
+            links = driver.find_elements_by_partial_link_text('More')
+            for link in links:
+                # Pass URL to reader
+                read_page(link.get_attribute('href'))
+
+            # Next page
+            try:
+                next_url = driver.find_element_by_partial_link_text('next')
+            except NoSuchElementException:
+                break
+            index += 1
+            driver.get(next_url.get_attribute('href'))
+
